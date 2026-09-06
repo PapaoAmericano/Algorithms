3984aa378120665c86e03bd4d70156336676f5ed
PapaoAmericano <maksim.ru1405@gmail.com>
2026-09-06 16:27:42 +0300

    lab1.c

--- Lab1C/lab1.c
+++ Lab1C/lab1.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
 #include "array.h"
 
-Array *array_create_and_read(FILE *input)
-{
+Array *array_create_and_read(FILE *input) {
     int n;
     fscanf(input, "%d", &n);
-    /* Create array */
     Array *arr = array_create(n, NULL);
-    /* Read array data */
-    for (int i = 0 ; i < n ; ++i)
-    {
+    for (int i = 0; i < n; ++i) {
         int x;
         fscanf(input, "%d", &x);
-        array_set(arr, i, x);
+        array_set(arr, i, (Data)x);
     }
+    
     return arr;
 }
 
-void task1(Array *arr)
-{
+void task1(Array *arr) {
+    size_t count = array_size(arr);
+    if (count > 5) count = 5;
+    for (size_t i = 0; i < count; ++i) {
+        printf("%lu ", array_get(arr, i));   
+    }
 }
 
-void task2(Array *arr)
-{
+void task2(Array *arr) {
 }
 
-int main(int argc, char **argv)
-{
-    Array *arr = NULL;
+int main(int argc, char **argv) {
+    if (argc < 2) 
+        return 1;
     FILE *input = fopen(argv[1], "r");
-    arr = array_create_and_read(input);
+    if (!input) 
+        return 1;
+
+    Array *arr = array_create_and_read(input);
     task1(arr);
     array_delete(arr);
-    /* Create another array here */
+
     arr = array_create_and_read(input);
     task2(arr);
     array_delete(arr);
+
     fclose(input);
+    return 0;
 }
