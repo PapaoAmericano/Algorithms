ec342b2940ac4f41869ad5cb36385f8d31acfb30
PapaoAmericano <maksim.ru1405@gmail.com>
2026-09-06 16:26:05 +0300

    array.c

--- LibraryC/array.c
+++ LibraryC/array.c
@@ -1,36 +1,53 @@
 #include <stdlib.h>
 #include "array.h"
 
-typedef struct Array {
-    // remove this
-    Data d;
-} Array;
+struct Array {
+    Data* data;       
+    size_t size;      
+    FFree* free_func; 
+};
 
-// create array
-Array *array_create(size_t size, FFree f)
-{
-    return malloc(sizeof(Array));
+Array* array_create(size_t size, FFree f) {
+    Array* arr = malloc(sizeof(Array));
+    if (!arr) return NULL;
+
+    arr->data = malloc(size * sizeof(Data));
+    if (!arr->data) {
+        free(arr);
+        return NULL;
+    }
+
+    arr->size = size;
+    arr->free_func = f;  
+    
+    return arr;
 }
 
-// delete array, free memory
-void array_delete(Array *arr)
-{
-    free(arr);
+void array_delete(Array* arr) {
+    if (!arr) 
+        return;
+    
+    free(arr->data);   
+    free(arr);         
 }
 
-// returns specified array element
-Data array_get(const Array *arr, size_t index)
-{
-    return (Data)0;
+Data array_get(const Array* arr, size_t index) {
+    if (!arr || index >= arr->size) 
+        return (Data)0;
+    
+    return arr->data[index];
 }
 
-// sets the specified array element to the value
-void array_set(Array *arr, size_t index, Data value)
-{
+void array_set(Array* arr, size_t index, Data value) {
+    if (!arr || index >= arr->size) 
+        return;
+    
+    arr->data[index] = value;
 }
 
-// returns array size
-size_t array_size(const Array *arr)
-{
-    return 0;
+size_t array_size(const Array* arr) {
+    if (!arr) 
+        return 0;
+    
+    return arr->size;
 }
