d0dd85161a9e0c7a4c0ff25501f231af601e35de
PapaoAmericano <maksim.ru1405@gmail.com>
2026-09-07 15:26:24 +0300

    array.c

--- LibraryC/array.c
+++ LibraryC/array.c
@@ -2,46 +2,62 @@
 #include "array.h"
 
 struct Array {
-    Data* data;
+    Data *data;
     size_t size;
+    FFree *free_func;
 };
 
-Array* array_create(size_t size, FFree f) {
-    Array* arr = malloc(sizeof(Array));
-    
+Array *array_create(size_t size, FFree f) {
+    Array *arr = malloc(sizeof(Array));
     if (!arr) 
         return NULL;
     
     arr->data = malloc(size * sizeof(Data));
-    
-    if (!arr->data) {
-        free(arr);
-        return NULL;
+    if (!arr->data) 
+    { 
+        free(arr); 
+        return NULL; 
     }
-
+    
     arr->size = size;
+    arr->free_func = f;
+    
+    for (size_t i = 0; i < size; ++i)
+        arr->data[i] = 0;
+    
     return arr;
 }
 
-void array_delete(Array* arr) {
-    if (arr) {
-        free(arr->data);
-        free(arr);
+void array_delete(Array *arr) {
+    if (!arr) return;
+    if (arr->free_func) {
+        for (size_t i = 0; i < arr->size; ++i) {
+            if (arr->data[i] != 0)
+                arr->free_func((void*)arr->data[i]);
+        }
     }
+    free(arr->data);
+    free(arr);
 }
 
-Data array_get(const Array* arr, size_t index) {
+Data array_get(const Array *arr, size_t index) {
     if (!arr || index >= arr->size) 
         return 0;
-
+    
     return arr->data[index];
 }
 
-void array_set(Array* arr, size_t index, Data value) {
-    if (arr && index < arr->size)
-        arr->data[index] = value;
+void array_set(Array *arr, size_t index, Data value) {
+    if (!arr || index >= arr->size) 
+        return;
+    
+    if (arr->free_func && arr->data[index] != 0) {
+        arr->free_func((void*)arr->data[index]);
+    }
+    
+    arr->data[index] = value;
 }
 
-size_t array_size(const Array* arr) {
+size_t array_size(const Array *arr) {
     return arr ? arr->size : 0;
 }
