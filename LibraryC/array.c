7768cba0d2ea25f301c74e2aea0d8ac50f37647b
PapaoAmericano <maksim.ru1405@gmail.com>
2026-09-07 15:18:31 +0300

    array.c

--- LibraryC/array.c
+++ LibraryC/array.c
@@ -2,52 +2,46 @@
 #include "array.h"
 
 struct Array {
-    Data* data;       
-    size_t size;      
-    FFree* free_func; 
+    Data* data;
+    size_t size;
 };
 
 Array* array_create(size_t size, FFree f) {
     Array* arr = malloc(sizeof(Array));
-    if (!arr) return NULL;
-
+    
+    if (!arr) 
+        return NULL;
+    
     arr->data = malloc(size * sizeof(Data));
+    
     if (!arr->data) {
         free(arr);
         return NULL;
     }
 
     arr->size = size;
-    arr->free_func = f;  
-    
     return arr;
 }
 
 void array_delete(Array* arr) {
-    if (!arr) 
-        return;
-    
-    free(arr->data);   
-    free(arr);         
+    if (arr) {
+        free(arr->data);
+        free(arr);
+    }
 }
 
 Data array_get(const Array* arr, size_t index) {
     if (!arr || index >= arr->size) 
-        return (Data)0;
-    
+        return 0;
+
     return arr->data[index];
 }
 
 void array_set(Array* arr, size_t index, Data value) {
-    if (!arr || index >= arr->size) 
-        return;
-    
-    arr->data[index] = value;
+    if (arr && index < arr->size)
+        arr->data[index] = value;
 }
 
 size_t array_size(const Array* arr) {
-    if (!arr) 
-        return 0;
-    
-    return arr->size;
+    return arr ? arr->size : 0;
 }
